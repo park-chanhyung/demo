60c68affde95a2098b1087147c0a28194ca9281c
Chanhyung-Park <chanhyung13@gmail.com>
2024-06-10 12:06:10 +0900

    추천 , 앵커 , 마크다운 , 검색 완료

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -11,4 +12,5 @@ public interface QuestionRepository extends JpaRepository<Question,Long> {
 
     Question findBySubjectAndContent(String subject,String content);
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec ,Pageable pageable);
 }
